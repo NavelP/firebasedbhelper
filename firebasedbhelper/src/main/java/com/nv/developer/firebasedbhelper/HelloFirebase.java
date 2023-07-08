41181e116dff083da5f51c8257acdc927e2cfb96
NavelP <emmanuelfordaysankoh@gmail.com>
2023-07-08 18:55:04 +0300

    initial commit

---
+++ firebasedbhelper/src/main/java/com/nv/developer/firebasedbhelper/HelloFirebase.java
@@ -0,0 +1,9 @@
+package com.nv.developer.firebasedbhelper;
+
+public class HelloFirebase {
+    private static String text = "Hello firebase";
+
+    public static void PrintText(){
+        System.out.println(text);
+    }
+}
